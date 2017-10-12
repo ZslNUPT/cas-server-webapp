dbe8aecb0cc4e5c06c5348bbb1be357c54597e24
zuoshenglong <zuoshenglong@qq.com>
2017-10-12 20:08:10 +0800

    设置跨域允许angularjs重定向到中心认证服务器， 但是报CorsRegistry这个类classNotFound

---
+++ src/main/java/com/ecidi/dam/CorsConfigurerAdapter.java
@@ -0,0 +1,12 @@
+package com.ecidi.dam;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+public class CorsConfigurerAdapter extends WebMvcConfigurerAdapter {
+
+ @Override
+ public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/*").allowedOrigins("*");
+ }
+}
